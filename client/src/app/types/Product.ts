663460b5528ec2bc8eb103807ce0f034e551ea7a
PetarPetrov01 <petyr_p@mail.bg>
2026-05-09 16:14:02 +0300

    refactor(client): extend Product types with images[], slug, shortDescription, etc.

--- client/src/app/types/Product.ts
+++ client/src/app/types/Product.ts
@@ -3,27 +3,29 @@ import { User } from "./User";
 export interface Product {
   name: string;
   description: string;
-  image: string;
+  shortDescription: string;
+  images: string[];
   category: string[];
   style: string;
-  dimensions: {
-    height: number;
-    width: number;
-    depth: number;
-  };
+  dimensions: { height: number; width: number; depth: number };
   material: string[];
   color: string;
   price: number;
+  tags?: string[];
+  inStock?: boolean;
 }
 
-export interface APIProduct extends Product{
+export interface APIProduct extends Product {
   _id: string;
+  slug: string;
+  isFeatured: boolean;
+  inStock: boolean;
+  tags: string[];
   _ownerId: string;
   __v: string;
   createdAt: string;
 }
 
-export interface PopulatedProduct extends  Omit<APIProduct,'_ownerId'>{
+export interface PopulatedProduct extends Omit<APIProduct, "_ownerId"> {
   _ownerId: User;
 }
-
